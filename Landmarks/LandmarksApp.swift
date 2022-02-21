8eb265b1c4d6bfee7107380fb1215d0976f987d7
Seung Ju <kingsky32@gmail.com>
2022-02-21 20:04:31 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 오승주 on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
